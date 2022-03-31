2ab7ab6995cedf58296b7cea8869d899eb76853c
p2024398 <p2024398@univ-lyon1.fr>
2022-03-31 06:05:08 +0200

    shorter version

--- src/main.cpp
+++ src/main.cpp
@@ -10,47 +10,6 @@ using namespace std;
 
 int main()
 {
-    //int id, age;
-    //string name;
-    /*string name;
-    int price, pos, tempscuisson;
-    ifstream file("test.txt");
-    if(file.is_open())
-    {
-        string line;
-		//getline(file, line);
-        while(file >> name >> price >> pos >> tempscuisson)
-        {
-            cout << name << " " << price << " " << pos<< " " << tempscuisson << endl;
-        }
-
-        file.close();
-    }
-    else cout << "FAILED..." << endl;*/
-
-
-    /*ifstream fileIng("./txt/test.txt");
-    string nomIng;
-    int prix, pos, tempsCuisson;
-    if(fileIng.is_open()){
-
-        //ignorer la premiere ligne
-		//string line;
-		//getline(fileIng, line);
-
-        while(fileIng >> nomIng >> prix >> pos >> tempsCuisson){
-            //Ingredient ing(nomIng, prix, pos, 10, tempsCuisson);
-            //ingVec.push_back(ing);
-            cout << nomIng << " " << prix << " " << pos << " " << tempsCuisson << endl;
-        }
-
-
-        fileIng.close();
-    }
-
-    else 
-        cout << "Failed to open file..." << endl;*/
-    
     Jeu j1(0,0);
     //string ingredient = "Salade";
     j1.chargerIngredient("./txt/Ingredient.md");

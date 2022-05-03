498a9b0eeb928959e8088e106df3e9dd6729b289
HANNA SANDRINE p1906670 <p1906670@b710l0317.univ-lyon1.fr>
2022-05-03 10:44:51 +0200

    doxy client

--- src/Client.cpp
+++ src/Client.cpp
@@ -7,20 +7,24 @@
 using namespace  std;
 
 
+/** < @brief Constructeur de Client */ 
 Client::Client(const unsigned int idC, vector<Commande> & comm, unsigned int prix){
     idClient=idC;
     com=comm;
     prixTot=prix;
 }
 
+/** < @brief Destructeur de Client */ 
 Client::~Client(){
 
 }
 
+/** < @brief fonction qui retourne l'identifiant du Client */ 
 unsigned int Client::getIdC() const{
      return idClient;
  }
 
+/** < @brief fonction qui retourne le prix total de la commande du client */ 
  unsigned int Client::getPrix() const{
      return prixTot;
  }

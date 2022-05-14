1bebf01deb005b4e5b7519178d4ad2ee2b7a4f91
Sandrine <sandrine.hanna@etu.univ-lyon1.fr>
2022-05-15 00:47:21 +0200

    doxy txt

--- src/txt/txtJeu.h
+++ src/txt/txtJeu.h
@@ -3,9 +3,16 @@
 
 #include "../Jeu.h"
 
+/** < @brief permet d'afficher la carte */
 void affCarte(vector <Commande> carte);
+
+/** < @brief affiche la recette d'une commande */
 void affRecette(vector<Recette> tabRec);
+
+/** < @brief  */
 void txtBoucle (Jeu & j, vector<Recette> &R);
+
+/** < @brief affiche les ingredients */
 void affIng(vector<Ingredient> tabIng);
 
 #endif
